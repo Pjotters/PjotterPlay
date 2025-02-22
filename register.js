9cd43918b5c968598aeef76997b2bfa2e7790340
Pjotters-Chain <05.hek_domein@icloud.com>
2025-02-22 01:35:39 +0100

    register

--- register.js
+++ register.js
@@ -1,11 +1,12 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
-import { getFirestore, doc, setDoc } from 'firebase/firestore';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js';
+import { getFirestore, doc, setDoc } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
 
 // Gebruik de bestaande Firebase configuratie
 const firebaseConfig = {
     apiKey: "AIzaSyBCXaYJI9dxwqKD1Qsb_9AOdsnVTPG2uHM",
     authDomain: "pjotters-company.firebaseapp.com",
+    databaseURL: "https://pjotters-company-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "pjotters-company",
     storageBucket: "pjotters-company.appspot.com",
     messagingSenderId: "64413422793",
